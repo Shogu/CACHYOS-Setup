2acda46e3dff30f08d106501f5cbf47d669595a9
ogu <trevisiolugo@gmail.com>
2025-09-01 18:19:08 +0200

    Update maestral_install.sh

--- maestral_install.sh
+++ maestral_install.sh
@@ -1,30 +1,19 @@
-#!/bin/bash
-# Script d'installation propre de Maestral (avec GUI) sur Arch Linux Fish shell
-# Auteur : généré pour Ogu
-
-VENV="$HOME/.maestral-venv"
-MAESTRAL_WHL="$HOME/Téléchargements/maestral-1.9.4-py3-none-any.whl"
-
+#!/usr/bin/env fish
 echo "=== Suppression de l'ancien environnement virtuel ==="
-rm -rf "$VENV"
+rm -rf ~/.maestral-venv
 
-echo "=== Création d'un nouvel environnement virtuel caché ==="
-python -m venv "$VENV"
+echo "=== Création d'un nouvel environnement virtuel ==="
+python3 -m venv ~/.maestral-venv
 
 echo "=== Activation de l'environnement virtuel ==="
-# Fish shell
-source "$VENV/bin/activate.fish"
-
-echo "=== Téléchargement du fichier wheel Maestral ==="
-wget -O "$MAESTRAL_WHL" "https://files.pythonhosted.org/packages/df/6f/6f7f1f243f4c8f6b6b7c1234567890abcdef1234567890abcdef123456/maestral-1.9.4-py3-none-any.whl"
+source ~/.maestral-venv/bin/activate.fish
 
 echo "=== Mise à jour de pip ==="
 pip install --upgrade pip
 
 echo "=== Installation de Maestral avec GUI ==="
-pip install "$MAESTRAL_WHL"[gui] --force-reinstall
+pip install "maestral[gui]"
 
 echo "=== Installation terminée ==="
-echo "Pour lancer Maestral GUI depuis Fish shell :"
-echo "source $VENV/bin/activate.fish; maestral gui"
-
+echo "Pour lancer Maestral GUI :"
+echo "source ~/.maestral-venv/bin/activate.fish && maestral gui"
