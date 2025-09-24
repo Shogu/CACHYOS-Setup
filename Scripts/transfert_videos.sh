15629e13e183146d760ff63166efd055e9b407d8
ogu <trevisiolugo@gmail.com>
2025-09-24 13:09:44 +0200

    Update transfert_videos.sh

--- Scripts/transfert_videos.sh
+++ Scripts/transfert_videos.sh
@@ -7,26 +7,18 @@ set VIDEO_EXTENSIONS .mp4 .mkv .avi .mov .flv .wmv .mpeg .mpg .webm
 function move_and_cleanup
     set file $argv[1]
     set dest $argv[2]
-    echo "File found: $file"
 
-    read -P "Do you want to move this file to $dest ? (o/n) " choice
-    if test "$choice" = o -o "$choice" = O
-        mkdir -p "$dest"
-        mv "$file" "$dest"
-        echo "File moved to $dest"
+    echo "Déplacement : $file → $dest"
+    mkdir -p "$dest"
+    mv "$file" "$dest"
 
-        set dir (dirname "$file")
-        while test "$dir" != "$SOURCE_DIR"
-            if test ! -d "$dir"
-                rmdir "$dir"
-                echo "Folder removed: $dir"
-            else
-                break
-            end
-            set dir (dirname "$dir")
+    set dir (dirname "$file")
+    # Supprime uniquement le dossier s'il est vide
+    if test -d "$dir"
+        if test (count (ls -A "$dir")) -eq 0
+            rmdir "$dir"
+            echo "Dossier supprimé : $dir"
         end
-    else
-        echo "File ignored."
     end
 end
 
@@ -36,4 +28,4 @@ for ext in $VIDEO_EXTENSIONS
     end
 end
 
-echo "Processing completed."
+echo "Traitement terminé."
